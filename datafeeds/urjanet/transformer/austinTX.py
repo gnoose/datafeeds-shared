e954ea2ca11c41807e9967d5ab81e6b32be4da34
Cristian Jansenson <cristian@gridium.com>
2019-10-11 10:47:22 -0400

    Pylint

--- datafeeds/urjanet/transformer/austinTX.py
+++ datafeeds/urjanet/transformer/austinTX.py
@@ -5,10 +5,6 @@ from datafeeds.urjanet.model import Account
 
 class AustinTXBillingPeriod(GenericWaterBillingPeriod):
     def get_total_charge(self):
-        # Account.NewCharges is not set; collect from floating_charges instead
-        return sum([charge.ChargeAmount for charge in self.account.floating_charges])
-
-    def get_total_charge(self):
         total_charges = 0
         for meter in self.account.meters:
             for charges in meter.charges:
