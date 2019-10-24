83a78c51fd8eeef1763d0d2a561888e1c4e429fe
Kimberly Nicholls <1649528+kielni@users.noreply.github.com>
2019-10-24 09:27:01 -0700

    data file name

--- datafeeds/urjanet/tests/test_pleasanton_transformer.py
+++ datafeeds/urjanet/tests/test_pleasanton_transformer.py
@@ -21,7 +21,7 @@ class TestUrjanetPleasantonTransformer(test_util.UrjaFixtureText):
 
     def test_4097_fixture(self):
         """Tests the water transformer on account Pleasanton."""
-        self.pleasanton_fixture_test("4097_input.json", "4097_expected.json")
+        self.pleasanton_fixture_test("input_4097.json", "expected_4097.json")
 
 
 if __name__ == "__main__":
