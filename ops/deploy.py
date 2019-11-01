6eb06c61f77aadf388a4f5ebcbb41231d5518828
Joe Thomas <jsthomas@protonmail.com>
2019-11-01 09:20:37 -0600

    Minor tweaks to deploy script.

--- ops/deploy.py
+++ ops/deploy.py
@@ -16,6 +16,7 @@ from git import Repo
 import argparse
 import boto3
 import config
+from datafeeds.config import DATAFEEDS_ROOT
 import logging
 import slack
 import slack.chat
@@ -27,7 +28,7 @@ slack_channel = config.SLACK_CHANNEL
 
 log = logging.getLogger(__name__)
 
-ecr_client = boto3.client("ecr")
+ecr_client = boto3.client("ecr", region_name=config.AWS_REGION_NAME)
 
 
 def post_message(message, channel, icon=':mega:'):
@@ -41,15 +42,15 @@ def post_message(message, channel, icon=':mega:'):
         log.exception("Failed to post error message to slack. Channel: %s, Message: %s", channel, message)
 
 
-def get_commit_id(branch: str = "master") -> str:
-    repo = Repo()
+def get_commit_id(branch: str) -> str:
+    repo = Repo(DATAFEEDS_ROOT)
     # assumes remote is named "origin"
     log.info("Fetching latest from remote")
     repo.remotes.origin.fetch()
     branch_head = getattr(repo.heads, branch)
-    id = branch_head.commit.hexsha
-    log.info("Commit ID at head of %s: %s", branch_head, id)
-    return id
+    commit_id = branch_head.commit.hexsha
+    log.info("Commit ID at head of %s: %s", branch_head, commit_id)
+    return commit_id
 
 
 def find_image_tag(docker_tag: str, repo: str = "datafeeds") -> bool:
@@ -97,18 +98,15 @@ def main():
     args = parser.parse_args()
 
     if args.githash:
-        post_message("Datafeeds image re-tagging has started. (Git Hash: %s)" % args.githash, slack_channel)
+        how = "Git Hash: %s" % args.githash
         log.info("Attempting to retag the image with git hash %s.", args.githash)
         commit_id = args.githash
-    elif args.branch:
-        post_message("A webapps deploy has started. (Branch: %s)" % args.branch, slack_channel)
+    else:
+        how = "Branch: %s" % args.branch
         log.info("Attempting to deploy the image for branch %s.", args.branch)
         commit_id = get_commit_id(args.branch)
-    else:
-        post_message("A webapps deploy has started. (Branch: %s)" % args.branch, slack_channel)
-        log.info("Attempting to deploy the image for branch %s.", args.branch)
-        commit_id = get_commit_id()
 
+    post_message("A datafeeds deploy has started. (%s)" % how, slack_channel)
     image_tag_exists = find_image_tag(commit_id)
 
     if image_tag_exists:
@@ -117,8 +115,7 @@ def main():
             retag_image(commit_id, DEPLOY_TAG)
             log.info("Retagging image completed successfully for %s.", commit_id)
             post_message("Retagging image completed successfully for %s." % commit_id, slack_channel)
-
-        except Exception:
+        except Exception:  # noqa E722
             log.exception("Retagging image failed for %s", commit_id)
             post_message("Retagging image failed for %s" % commit_id, slack_channel, icon=":x:")
             sys.exit(1)
