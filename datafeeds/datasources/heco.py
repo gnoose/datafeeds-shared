e82c2f7fe25a1bc61bf889dcaebb3662350b473f
David Welch <david@gridium.com>
2019-11-06 12:25:39 -0600

    fix flake8 error

--- datafeeds/datasources/heco.py
+++ datafeeds/datasources/heco.py
@@ -10,7 +10,7 @@ from datafeeds.common.batch import run_urjanet_datafeed
 def datafeed(account: SnapmeterAccount, meter: Meter,
              datasource: MeterDataSource, params: dict,
              task_id: Optional[str] = None) -> Status:
-    
+
     return run_urjanet_datafeed(
         account,
         meter,
