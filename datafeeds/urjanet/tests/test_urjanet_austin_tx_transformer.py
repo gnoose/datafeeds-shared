5e5d1d2bbe39e7b7d4201388648a8f5011c34ee4
Kimberly Nicholls <1649528+kielni@users.noreply.github.com>
2019-11-13 11:27:53 -0800

    fix spacing

--- datafeeds/urjanet/tests/test_urjanet_austin_tx_transformer.py
+++ datafeeds/urjanet/tests/test_urjanet_austin_tx_transformer.py
@@ -28,5 +28,6 @@ class TestUrjanetAustinTXTransformer(test_util.UrjaFixtureText):
         self.austin_tx_fixture_test("input_0375958587.json", "expected_0375958587.json")
         self.austin_tx_fixture_test("input_8166749309.json", "expected_8166749309.json")
 
+
 if __name__ == "__main__":
     unittest.main()
