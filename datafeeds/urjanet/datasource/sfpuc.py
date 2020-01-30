28f6c39a27e41d149f3d059fc1a5f75146412886
Kimberly Nicholls <1649528+kielni@users.noreply.github.com>
2020-01-30 13:49:13 -0800

    set up for porting SFPUC

--- datafeeds/urjanet/datasource/sfpuc.py
+++ datafeeds/urjanet/datasource/sfpuc.py
@@ -1,41 +1,45 @@
-from typing import List
+from typing import Optional, List
 
+from datafeeds.common.batch import run_urjanet_datafeed
+from datafeeds.common.typing import Status
+from datafeeds.models import (
+    SnapmeterAccount,
+    Meter,
+    SnapmeterMeterDataSource as MeterDataSource,
+)
 from datafeeds.urjanet.datasource.pymysql_adapter import UrjanetPyMySqlDataSource
-from datafeeds.urjanet.model import Account, Meter
+from datafeeds.urjanet.model import Account
 
 
-class SfpucWaterDatasource(UrjanetPyMySqlDataSource):
-    """Load San Francisco Public Utlity Commission (SFPUC) water data from an Urjanet database
+class SanFranciscoWaterDatasource(UrjanetPyMySqlDataSource):
+    """Load data from an Urjanet database"""
 
-    This class accepts an account number and an optional meter number. If no meter number is specified, all meters
-    are loaded from each bill.
-    """
-
-    def __init__(self, account_number: str, meter_number: str = None):
+    def __init__(self, account_number: str):
         super().__init__(account_number)
-        self.meter_number = meter_number
+        self.account_number = self.normalize_account_number(account_number)
 
     def load_accounts(self) -> List[Account]:
-        """Load accounts based on the account id"""
-        query = """
-            SELECT *
-            FROM Account
-            WHERE AccountNumber=%s AND UtilityProvider = 'SanFranciscoPublicUtilities'
-        """
-        result_set = self.fetch_all(query, self.account_number)
-        return [UrjanetPyMySqlDataSource.parse_account_row(row) for row in result_set]
+        """The query for fetching accounts must be provided by implementers"""
+        pass  # TODO: update this
 
-    def load_meters(self, account_pk: str) -> List[Meter]:
-        """Load meters for an account, optionally filtering by meter ID
+    def load_meters(self, account_pk: int) -> List[Meter]:
+        """The query for fetching meters must be provided by implementers"""
+        pass  # TODO: update this
 
-        Currently, both water and sewer meters are loaded.
-        """
 
-        query = "SELECT * FROM Meter WHERE ServiceType in ('water', 'sewer') AND AccountFK=%s"
-        if self.meter_number:
-            query = query + " AND MeterNumber='%s'"
-            result_set = self.fetch_all(query, account_pk, self.meter_number)
-        else:
-            result_set = self.fetch_all(query, account_pk)
-
-        return [UrjanetPyMySqlDataSource.parse_meter_row(row) for row in result_set]
+def datafeed(
+    account: SnapmeterAccount,
+    meter: Meter,
+    datasource: MeterDataSource,
+    params: dict,
+    task_id: Optional[str] = None,
+) -> Status:
+    return run_urjanet_datafeed(
+        account,
+        meter,
+        datasource,
+        params,
+        SanFranciscoWaterDatasource(meter.utility_account_id),
+        SanFranciscoWaterTransformer(),
+        task_id=task_id,
+    )
--- datafeeds/urjanet/transformer/sfpuc.py
+++ datafeeds/urjanet/transformer/sfpuc.py
@@ -1,44 +1,15 @@
-"""San Francisco Public Utility Commission (SFPUC) Urjanet water transformer"""
-
-import logging
-from decimal import Decimal
-
-from datafeeds.urjanet.transformer import GenericBillingPeriod, GenericWaterTransformer
+from datafeeds.urjanet.transformer import (
+    GenericBillingPeriod,
+    UrjanetGridiumTransformer,
+)
 from datafeeds.urjanet.model import Account
 
 
-log = logging.getLogger(__name__)
+class SanFranciscoWaterBillingPeriod(GenericBillingPeriod):
+    pass  # TODO: remove if not needed
 
 
-class SfpucWaterBillingPeriod(GenericBillingPeriod):
-    """Simple model of a SFPUC water billing period.
-
-    This is an exceptionally simple model. Each billing period is represented by an Urjanet
-    "account" object. This design is based on the limited billing data we've received so far through
-    Urjanet, and might need to change as we support more SFPUC water customers.
-
-    Currently, both water and sewer charges are included in the billing period.
-    """
-
-    def iter_usages(self):
-        # We only look at water meters because some bills also have "sewer" meters that also
-        # report usage, but it's just some percentage of the water meter usage and therefore
-        # redundant.
-        for meter in self.account.meters:
-            if meter.ServiceType == "water":
-                for usage in meter.usages:
-                    yield usage
-
-    def get_total_usage(self) -> Decimal:
-        def filter_for_total(usage):
-            return usage.RateComponent == "[total]"
-
-        return Decimal(
-            sum([u.UsageAmount for u in self.iter_usages() if filter_for_total(u)])
-        )
-
-
-class SfpucWaterTransformer(GenericWaterTransformer):
+class SanFranciscoWaterTransformer(UrjanetGridiumTransformer):
     @staticmethod
-    def billing_period(account: Account) -> SfpucWaterBillingPeriod:
-        return SfpucWaterBillingPeriod(account)
+    def billing_period(account: Account,) -> SanFranciscoWaterBillingPeriod:
+        return SanFranciscoWaterBillingPeriod(account)
