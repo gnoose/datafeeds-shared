ed71c53a484fc026a98cef597f535fec3d92ca8c
huzaifah0x00 <huzaifashaikh0012@gmail.com>
2020-04-24 02:18:55 +0500

    add FPL MyAccount selenium scraper

--- datafeeds/scrapers/tests/test_fpl_myaccount.py
+++ datafeeds/scrapers/tests/test_fpl_myaccount.py
@@ -4,21 +4,24 @@ from datetime import date
 from dateutil import parser as date_parser
 
 from datafeeds.common.support import Credentials, DateRange
-from fpl_myaccount import FPLMyAccountConfiguration, FPLMyAccountScraper
+from datafeeds.scrapers.fpl_myaccount import (
+    FPLMyAccountConfiguration,
+    FPLMyAccountScraper,
+)
 
 """
     Run this to launch the FPL MyAccount scraper:
 
     $ export PYTHONPATH=$(pwd)
-    $ python datafeeds/scrapers/tests/test_fpl_my_account.py utility_acct start end username 
+    $ python datafeeds/scrapers/tests/test_fpl_my_account.py utility_acct start end username
     password
 """
 
 
 def test_scraper(
-    utility_account: str, start_date: date, end_date: date, username: str, password: str
+    account_number: str, start_date: date, end_date: date, username: str, password: str
 ):
-    configuration = FPLMyAccountConfiguration(utility_account=utility_account)
+    configuration = FPLMyAccountConfiguration(account_number=account_number)
     credentials = Credentials(username, password)
     scraper = FPLMyAccountScraper(
         credentials, DateRange(start_date, end_date), configuration
@@ -32,16 +35,16 @@ def test_scraper(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("utility_account", type=str)
+    parser.add_argument("account_number", type=str)
     parser.add_argument("start", type=str)
     parser.add_argument("end", type=str)
     parser.add_argument("username", type=str)
     parser.add_argument("password", type=str)
     args = parser.parse_args()
     test_scraper(
-        args.utility_account,
-        date_parser.parse(args.start),
-        date_parser.parse(args.end),
+        args.account_number,
+        date_parser.parse(args.start, dayfirst=True).date(),
+        date_parser.parse(args.end, dayfirst=True).date(),
         args.username,
         args.password,
     )
